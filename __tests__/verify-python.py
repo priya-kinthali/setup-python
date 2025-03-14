022b88cf4c19ca13d8355029b902cdeaae11b317
priya-kinthali <priya-kinthali@github.com>
2025-03-14 16:04:01 +0530

    intial commit

--- __tests__/verify-python.py
+++ __tests__/verify-python.py
@@ -1,14 +1,12 @@
 import sys
 argCount = len(sys.argv) - 1
-print(f"argCount: " +  argCount + " is ")
 
 if argCount == 1:
     expectedVersion = sys.argv[1]
-    print(f"expectedVersion: " +  expectedVersion + " is ")
+    if expectedVersion.endswith('t'):
+        expectedVersion = expectedVersion[:-1]
     versions = len(expectedVersion.split("."))
-    print("versions: " +  versions + " is ")
     majorMinor = str(sys.version_info[0]) + '.' + str(sys.version_info[1])
-    print("majorMinor: " + majorMinor + "is ")
 
     if versions == 2:
         # Test only major and minor version
@@ -17,15 +15,10 @@ if argCount == 1:
     elif versions == 3:
         # Test major, minor and micro version
         majorMinorMicro = majorMinor + '.' + str(sys.version_info[2])
-        print("majorMinorMicro: " + majorMinorMicro + " is")
         if expectedVersion != majorMinorMicro:
             raise Exception("Incorrect major + minor + micro version detected\nExpected: " + expectedVersion + "\nActual: " + majorMinorMicro)
     else: 
         raise Exception("Incorrect number of arguments supplied")
     print("Correct version of Python " + expectedVersion + " detected")
-    print(expectedVersion)
-    # print("expectedVersion: " +  expectedVersion + " is ")
-    # print("versions: " +  versions + " is ")
-    # print("majorMinor: " + majorMinor + "is ")
 else:
     raise Exception("Incorrect number of arguments supplied")
