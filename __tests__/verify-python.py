18c9b31627545277fd501deb07af0b944708c3a8
priya-kinthali <priya-kinthali@github.com>
2025-03-14 14:48:17 +0530

    intial commit

--- __tests__/verify-python.py
+++ __tests__/verify-python.py
@@ -14,11 +14,15 @@ if argCount == 1:
     if versions == 2:
         # Test only major and minor version
         if expectedVersion != majorMinor:
+            print("Expected: " + expectedVersion)
+            print("Actual: " + majorMinor)
             raise Exception("Incorrect major + minor version detected\nExpected: " + expectedVersion + "\nActual: " + majorMinor)
     elif versions == 3:
         # Test major, minor and micro version
         majorMinorMicro = majorMinor + '.' + str(sys.version_info[2])
         if expectedVersion != majorMinorMicro:
+            print("Expected: " + expectedVersion)
+            print("Actual: " + majorMinorMicro)
             raise Exception("Incorrect major + minor + micro version detected\nExpected: " + expectedVersion + "\nActual: " + majorMinorMicro)
     else: 
         raise Exception("Incorrect number of arguments supplied")
