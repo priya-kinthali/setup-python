0d8f9f639573258dfd7830e40812560dcfc7aa9c
priya-kinthali <priya-kinthali@github.com>
2025-03-14 13:54:02 +0530

    verify python

--- __tests__/verify-python.py
+++ __tests__/verify-python.py
@@ -5,6 +5,10 @@ if argCount == 1:
     expectedVersion = sys.argv[1]
     versions = len(expectedVersion.split("."))
     majorMinor = str(sys.version_info[0]) + '.' + str(sys.version_info[1])
+    # Check if the expected version has a 't' suffix
+    isFreethreaded = expectedVersion.endswith('t')
+    if isFreethreaded:
+        expectedVersion = expectedVersion[:-1]  # Remove the 't' suffix for comparison
 
     if versions == 2:
         # Test only major and minor version
@@ -17,6 +21,8 @@ if argCount == 1:
             raise Exception("Incorrect major + minor + micro version detected\nExpected: " + expectedVersion + "\nActual: " + majorMinorMicro)
     else: 
         raise Exception("Incorrect number of arguments supplied")
-    print("Correct version of Python " + expectedVersion + " detected")
-else:
+    if isFreethreaded:
+        print("Correct freethreaded version of Python " + expectedVersion + "t detected")
+    else:
+        print("Correct version of Python " + expectedVersion + " detected")else:
     raise Exception("Incorrect number of arguments supplied")
