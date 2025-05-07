500b31b4ab3ed273cdfbb415426e915ce8f91c6a
priya-kinthali <priya-kinthali@github.com>
2025-05-08 00:06:30 +0530

    test 1079

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -96730,8 +96730,7 @@ const TOKEN = core.getInput('token');
 const AUTH = !TOKEN ? undefined : `token ${TOKEN}`;
 const MANIFEST_REPO_OWNER = 'priya-kinthali';
 const MANIFEST_REPO_NAME = 'python-versions';
-// const MANIFEST_REPO_BRANCH = 'update-versions-manifest-file';
-const MANIFEST_REPO_BRANCH = 'test-1079';
+const MANIFEST_REPO_BRANCH = 'update-versions-manifest-file';
 exports.MANIFEST_URL = `https://raw.githubusercontent.com/${MANIFEST_REPO_OWNER}/${MANIFEST_REPO_NAME}/${MANIFEST_REPO_BRANCH}/versions-manifest.json`;
 function findReleaseFromManifest(semanticVersionSpec, architecture, manifest) {
     return __awaiter(this, void 0, void 0, function* () {
